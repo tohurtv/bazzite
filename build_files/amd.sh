2b630cf66c57944c8d5828ba3108c537b23970c7
Tohur <41875527+tohurtv@users.noreply.github.com>
2025-04-07 15:55:08 -0600

    Update amd.sh

--- build_files/amd.sh
+++ build_files/amd.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Remove packages
+#dnf5 remove -y \
+       #discover-overlay \
+       
+# Install packages
+dnf5 install -y \
+       rocminfo \
+       rocm-opencl \
+       rocm-clinfo \
+       rocm-hip \
+       pamixer
